eaec9c80c931e2390c15cd0da4ef2134e6b66110
Seyit Yılmaz <30367777+SeyitYilmaz@users.noreply.github.com>
2020-04-30 17:42:18 +0300

    Soru getirtme yapildi

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Soru_Cevap.Models;
+
+namespace Soru_Cevap.Controllers
+{
+    public class HomeController : Controller
+    {
+        dbSoruCevapEntities db = new dbSoruCevapEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Soru> soru = db.Soru.ToList();
+            return View(soru);
+        }
+
+        public ActionResult Kategoriler()
+        {
+            List<Kategori> kategori = db.Kategori.ToList();
+            return PartialView(kategori);
+        }
+    }
+}
